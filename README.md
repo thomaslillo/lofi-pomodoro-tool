de37e4f71a28a28e2c192b10648d2d74c4cb34ea
thomaslillo <thomas.j.lillo@gmail.com>
2023-02-05 12:45:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lofi-pomodoro-tool
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-v73cx8)
---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Lofi Pomodoro</h1>
+        <p>
+          Super minimal so no distractions, always free, always ready for
+          learning.
+        </p>
+      </div>
+
+      <div></div>
+
+      <div>
+        <p>
+          Use the time below to set a countdown for each session, and you can
+          play some cool lofi beats with the embedded radio.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
